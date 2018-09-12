97d7ba9ccf80551957642a276a49c500a4253457
yc.Hou <522245189@qq.com>
2018-09-12 10:31:10 +0800

    sa

--- other/install1Go.sh
+++ other/install1Go.sh
@@ -4,11 +4,11 @@ echo "===== 将go1.10.2.linux-amd64.tar.gz 文件拷贝到/usr/local 下 ====="
 cp -Rf other/goinstallfile/go /usr/local
 #tar -C /usr/local -xzf go1.10.2.linux-amd64.tar.gz
 
-echo" 创建 go 工作目录 src\pak\bin"
-echo" .profile 追加PATH=/usr/local/go/bin"
-echo" .bashrc  末尾 export GOPATH=/home/car/carGo"
-echo"             export GOBIN=$GOPATH/bin"
-echo"/root/.bashrc  末尾 PATH=/usr/local/go/bin"
-echo"/root/.bashrc  末尾 export GOPATH=/home/car/carGo"
-echo"/root/.bashrc  末尾 export GOBIN=$GOPATH/bin"
+echo " 创建 go 工作目录 src\pak\bin"
+echo " .profile 追加PATH=/usr/local/go/bin"
+echo " .bashrc  末尾 export GOPATH=/home/car/carGo"
+echo "             export GOBIN=$GOPATH/bin"
+echo "/root/.bashrc  末尾 PATH=/usr/local/go/bin"
+echo "/root/.bashrc  末尾 export GOPATH=/home/car/carGo"
+echo "/root/.bashrc  末尾 export GOBIN=$GOPATH/bin"
 
