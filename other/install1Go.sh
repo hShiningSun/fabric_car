8c8f82d0ed76aa8a93449a4843e3ae231f0bbf06
yc.Hou <522245189@qq.com>
2018-09-12 10:52:24 +0800

    modify install1go

--- other/install1Go.sh
+++ other/install1Go.sh
@@ -2,7 +2,10 @@
 echo "===== 将go1.10.2.linux-amd64.tar.gz 文件拷贝到/usr/local 下 ====="
 #cp -Rf other/goinstallfile/go1.10.2.linux-amd64.tar.gz /usr/local
 # cp -Rf other/goinstallfile/go /usr/local
-tar -C /usr/local -xzf go1.10.2.linux-amd64.tar.gz
+(
+    cd /usr/local
+    tar -C /usr/local -xzf go1.10.2.linux-amd64.tar.gz
+)
 
 echo " 创建 go 工作目录 src\pak\bin"
 echo " .profile 追加PATH=/usr/local/go/bin"
