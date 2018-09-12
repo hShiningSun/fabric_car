51f020a73afe313618dfd7b8d68cc014f4bd374c
yc.Hou <522245189@qq.com>
2018-09-12 21:32:35 +0800

    asd

--- other/install3fabric.sh
+++ other/install3fabric.sh
@@ -8,7 +8,7 @@ echo "===== 只是提示你现在没有包 ======"
 echo "==================================================="
 echo "================cd fabric/script==================="
 echo "================modify version====================="
-echo "================start bootstart====================="
+echo "================start bootstart,no have up====================="
 echo "================git checkout v1.1.0  先切换了就下载不了 二进制文件 和fabric-sample===================="
 echo "================here if images download incomplete，download in start container  ==============="
 
