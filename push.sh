8ce69b6ffcc654e84d7910ff2bf5fb81a8fa22a2
yc.Hou <522245189@qq.com>
2018-09-13 15:02:01 +0800

    修改push文件

--- push.sh
+++ push.sh
@@ -4,7 +4,40 @@ echo "please input message "
 exit 1
 fi
 
+
+set -x
 git status
+res=$?
+set +x
+   if [ $res -ne 0 ]; then
+     echo "Failed to generate ${MESSAGE} config material..."
+     exit 1
+   fi
+
+set -x
 git add .
+res=$?
+set +x
+    if [ $res -ne 0 ]; then
+        echo "Failed to generate ${MESSAGE} config material..."
+        exit 1
+    fi
+
+set -x
 git commit -m "${MESSAGE}"
+res=$?
+set +x
+    if [ $res -ne 0 ]; then
+        echo "Failed to generate ${MESSAGE} config material..."
+        exit 1
+    fi
+
+set -x
 git push origin yun
+res=$?
+set +x
+    if [ $res -ne 0 ]; then
+        echo "Failed to generate ${MESSAGE} config material..."
+        exit 1
+    fi
+
