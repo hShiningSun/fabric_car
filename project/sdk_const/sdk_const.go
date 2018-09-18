030097c57280d780562ecc78da24d400ae2b4469
yc.Hou <522245189@qq.com>
2018-09-18 15:26:34 +0800

    addServer1

--- project/sdk_const/sdk_const.go
+++ project/sdk_const/sdk_const.go
@@ -54,7 +54,7 @@ const (
 	// ChannelName channelName
 	ChannelName = "mychannel"
 	// ChannelConfigPath 频道的配置文件，一开始创世纪生成的
-	ChannelConfigPath = "${GOPATH}/src/fabric_car/GenesisFile/channel-artifacts/mychannel.tx"
+	ChannelConfigPath = "../../../GenesisFile/channel-artifacts/mychannel.tx"
 )
 
 const (
