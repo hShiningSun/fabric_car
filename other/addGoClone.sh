455936f54d68d31d4318ea775e35cbac1620f1c6
yc.Hou <522245189@qq.com>
2018-11-06 10:35:45 +0800

    addgoget

--- other/addGoClone.sh
+++ other/addGoClone.sh
@@ -1,7 +1,8 @@
-git clone https://github.com/golang/sys.git   $GOPATH/src/golang.org/x/sys
-git clone https://github.com/grpc/grpc-go.git $GOPATH/src/google.golang.org/grpc
-git clone https://github.com/golang/net.git   $GOPATH/src/golang.org/x/net
-git clone https://github.com/golang/text.git  $GOPATH/src/golang.org/x/text
+git clone https://github.com/golang/sys.git    $GOPATH/src/golang.org/x/sys
+git clone https://github.com/grpc/grpc-go.git  $GOPATH/src/google.golang.org/grpc
+git clone https://github.com/golang/net.git    $GOPATH/src/golang.org/x/net
+git clone https://github.com/golang/text.git   $GOPATH/src/golang.org/x/text
+git clone https://github.com/golang/crypto.git $GOPATH/src/golang.org/x/crypto
 go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
 git clone https://github.com/google/go-genproto.git $GOPATH/src/google.golang.org/genproto
 echo "cd $GOPATH/src/"
